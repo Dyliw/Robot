c0ea578ea100028b61378fb29a3c0928ad302e30
Deysy <deysygmtz.dm@gmail.com>
2026-08-06 21:54:25 -0600

    Create display_config.h

---
+++ src/core/display_config.h
@@ -0,0 +1,37 @@
+#ifndef DISPLAY_CONFIG_H
+#define DISPLAY_CONFIG_H
+
+// Configuración de hardware
+#define PIN_SCK  12
+#define PIN_MOSI 11
+#define PIN_CS   8
+#define PIN_DC   9
+#define PIN_RES  10
+
+// Dimensiones de la pantalla
+#define DISPLAY_WIDTH  128
+#define DISPLAY_HEIGHT 64
+
+// Configuración de la cara
+#define FACE_CENTER_X  64
+#define FACE_CENTER_Y  32
+#define FACE_RADIUS    28
+
+// Configuración de ojos
+#define EYE_LEFT_X     42
+#define EYE_RIGHT_X    86
+#define EYE_Y          25
+#define EYE_RADIUS     6
+#define PUPIL_RADIUS   2
+
+// Configuración de boca
+#define MOUTH_CENTER_X 64
+#define MOUTH_CENTER_Y 50
+#define MOUTH_WIDTH    20
+#define MOUTH_HEIGHT   10
+
+// Configuración de cejas
+#define EYEBROW_Y      18
+#define EYEBROW_LENGTH 10
+
+#endif
